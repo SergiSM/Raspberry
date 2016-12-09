f061ac2f2c62b8d3dd644e98c4656b16609f306b
Sergi <sergi.sanchez.morato@gmail.com>
2016-12-09 12:52:58 +0100

    .

--- Phonegap - Flask/flask_web.py
+++ Phonegap - Flask/flask_web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import time
 
 app = Flask(__name__)
@@ -13,6 +13,14 @@ def seg():
     #return render_template('segons.html',segons=time.strftime("%S"))
     return (time.strftime("%S")) 
     #return (time.strftime("%H:%M:%S"))
+	
+@app.route('/activate')
+def activate():
+	fo = open("foo.txt", "w")
+	text = request.args.get('text')
+	fo.write(text)
+	fo.close()
+	return "OK"
 
 if __name__ == '__main__':
     app.run(
