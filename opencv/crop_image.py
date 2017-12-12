a4bc6172b46d7cb8142226123fa46ee9b341ba1e
SergiSM <SergiSM@users.noreply.github.com>
2017-12-12 17:26:48 +0100

    Update crop_image.py

--- opencv/crop_image.py
+++ opencv/crop_image.py
@@ -1,6 +1,8 @@
+
 import cv2
-img = cv2.imread("lenna.png")
-crop_img = img[200:400, 100:300] # Crop from x, y, w, h -> 100, 200, 300, 400
+img = cv2.imread("captura_pantalla.png")
+crop_img = img[100:700, 800:1200] # Crop from x, y, w, h -> 100, 200, 300, 400
 # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
 cv2.imshow("cropped", crop_img)
+cv2.imwrite("captura_pantalla_crop.png", crop_img)
 cv2.waitKey(0)
