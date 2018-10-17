e3237b58962974907592af0420bbc1e1124b1bf2
SergiSM <SergiSM@users.noreply.github.com>
2018-10-17 15:39:01 +0200

    Create llegir_i2c_mateixa_adreca.py

---
+++ I2C/llegir_i2c_mateixa_adreca.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+# I modified code from controle everything to use i2c bus 2 and 3
+# Original work: https://github.com/ControlEverythingCommunity/BMP280/blob/master/Python/BMP280.py
+# If you run the un modified code, you will see this method is much faster than using
+# the TCA9548A I2C Multiplexer
+# This code will only work if i2c is enabled and the following lines was added to /boot/config.txt
+# dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=2
+# dtoverlay=i2c-gpio,bus=4,i2c_gpio_delay_us=2,i2c_gpio_sda=17,i2c_gpio_scl=27
+
+#dtoverlay=i2c-gpio,i2c_gpio_delay_us=1 #GPIO 23 as SDA and GPIO 24 as SCL (GPIO 23 and 24 is defaults)
+# dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=1,i2c_gpio_sda=17,i2c_gpio_scl=27
+
+import smbus
+import time
+
+import bme680
+
+############## BME280 ####################
+# Get I2C bus
+bus3 = smbus.SMBus(3)
+#bus4 = smbus.SMBus(4)
+delay=0.05
+
+
+############## BME680 ####################
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except IOError:
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+# These calibration data can safely be commented
+# out, if desired.
+
+print('Calibration data:')
+for name in dir(sensor.calibration_data):
+
+    if not name.startswith('_'):
+        value = getattr(sensor.calibration_data, name)
+
+        if isinstance(value, int):
+            print('{}: {}'.format(name, value))
+
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
+
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+print('\n\nInitial reading:')
+for name in dir(sensor.data):
+    value = getattr(sensor.data, name)
+
+    if not name.startswith('_'):
+        print('{}: {}'.format(name, value))
+
+sensor.set_gas_heater_temperature(320)
+sensor.set_gas_heater_duration(150)
+sensor.select_gas_heater_profile(0)
+
+# Up to 10 heater profiles can be configured, each
+# with their own temperature and duration.
+# sensor.set_gas_heater_profile(200, 150, nb_profile=1)
+# sensor.select_gas_heater_profile(1)
+
+
+while True:
+    ############## BME280 ####################
+    #Reading Data from i2c bus3 3
+    b1 = bus3.read_i2c_block_data(0x77, 0x88, 24)
+
+    # Convert the data
+    # Temp coefficents
+    dig_T1 = b1[1] * 256 + b1[0]
+    dig_T2 = b1[3] * 256 + b1[2]
+    if dig_T2 > 32767 :
+        dig_T2 -= 65536
+    dig_T3 = b1[5] * 256 + b1[4]
+    if dig_T3 > 32767 :
+        dig_T3 -= 65536
+
+    # Pressure coefficents
+    dig_P1 = b1[7] * 256 + b1[6]
+    dig_P2 = b1[9] * 256 + b1[8]
+    if dig_P2 > 32767 :
+        dig_P2 -= 65536
+    dig_P3 = b1[11] * 256 + b1[10]
+    if dig_P3 > 32767 :
+        dig_P3 -= 65536
+    dig_P4 = b1[13] * 256 + b1[12]
+    if dig_P4 > 32767 :
+        dig_P4 -= 65536
+    dig_P5 = b1[15] * 256 + b1[14]
+    if dig_P5 > 32767 :
+        dig_P5 -= 65536
+    dig_P6 = b1[17] * 256 + b1[16]
+    if dig_P6 > 32767 :
+        dig_P6 -= 65536
+    dig_P7 = b1[19] * 256 + b1[18]
+    if dig_P7 > 32767 :
+        dig_P7 -= 65536
+    dig_P8 = b1[21] * 256 + b1[20]
+    if dig_P8 > 32767 :
+        dig_P8 -= 65536
+    dig_P9 = b1[23] * 256 + b1[22]
+    if dig_P9 > 32767 :
+        dig_P9 -= 65536
+
+    # BMP280 address, 0x76(118)
+    # Select Control measurement register, 0xF4(244)
+    #		0x27(39)	Pressure and Temperature Oversampling rate = 1
+    #					Normal mode
+    bus3.write_byte_data(0x77, 0xF4, 0x27)
+    # BMP280 address, 0x76(118)
+    # Select Configuration register, 0xF5(245)
+    #		0xA0(00)	Stand_by time = 1000 ms
+    bus3.write_byte_data(0x77, 0xF5, 0xA0)
+
+    time.sleep(delay)
+
+    # BMP280 address, 0x76(118)
+    # Read data back from 0xF7(247), 8 bytes
+    # Pressure MSB, Pressure LSB, Pressure xLSB, Temperature MSB, Temperature LSB
+    # Temperature xLSB, Humidity MSB, Humidity LSB
+    data = bus3.read_i2c_block_data(0x77, 0xF7, 8)
+
+    # Convert pressure and temperature data to 19-bits
+    adc_p = ((data[0] * 65536) + (data[1] * 256) + (data[2] & 0xF0)) / 16
+    adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
+
+    # Temperature offset calculations
+    var1 = ((adc_t) / 16384.0 - (dig_T1) / 1024.0) * (dig_T2)
+    var2 = (((adc_t) / 131072.0 - (dig_T1) / 8192.0) * ((adc_t)/131072.0 - (dig_T1)/8192.0)) * (dig_T3)
+    t_fine = (var1 + var2)
+    cTemp = (var1 + var2) / 5120.0
+    fTemp = cTemp * 1.8 + 32
+
+    # Pressure offset calculations
+    var1 = (t_fine / 2.0) - 64000.0
+    var2 = var1 * var1 * (dig_P6) / 32768.0
+    var2 = var2 + var1 * (dig_P5) * 2.0
+    var2 = (var2 / 4.0) + ((dig_P4) * 65536.0)
+    var1 = ((dig_P3) * var1 * var1 / 524288.0 + ( dig_P2) * var1) / 524288.0
+    var1 = (1.0 + var1 / 32768.0) * (dig_P1)
+    p = 1048576.0 - adc_p
+    p = (p - (var2 / 4096.0)) * 6250.0 / var1
+    var1 = (dig_P9) * p * p / 2147483648.0
+    var2 = p * (dig_P8) / 32768.0
+    pressure = (p + (var1 + var2 + (dig_P7)) / 16.0) / 100
+
+    print("############## BME280 ####################")
+    # Output data to screen
+    print("Reading sensor on i2c bus 3")
+    print "Temperature in Celsius : %.2f C" %cTemp
+    print "Temperature in Fahrenheit : %.2f F" %fTemp
+    print "Pressure : %.2f hPa " %pressure+"\n"
+
+    ############## BME680 ####################
+    print("############## BME680 ####################")
+    if sensor.get_sensor_data():
+        output = '{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH'.format(
+            sensor.data.temperature,
+            sensor.data.pressure,
+            sensor.data.humidity)
+
+        if sensor.data.heat_stable:
+            print('{0},{1} Ohms'.format(
+                output,
+                sensor.data.gas_resistance))
+
+        else:
+            print(output)
+
+    time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #Reading BMP280 on i2c bus3 4
+        # BMP280 address, 0x76(118)
+    # Read data back from 0x88(136), 24 bytes
+    '''b1 = bus4.read_i2c_block_data(0x76, 0x88, 24)
+
+    # Convert the data
+    # Temp coefficents
+    dig_T1 = b1[1] * 256 + b1[0]
+    dig_T2 = b1[3] * 256 + b1[2]
+    if dig_T2 > 32767 :
+        dig_T2 -= 65536
+    dig_T3 = b1[5] * 256 + b1[4]
+    if dig_T3 > 32767 :
+        dig_T3 -= 65536
+
+    # Pressure coefficents
+    dig_P1 = b1[7] * 256 + b1[6]
+    dig_P2 = b1[9] * 256 + b1[8]
+    if dig_P2 > 32767 :
+        dig_P2 -= 65536
+    dig_P3 = b1[11] * 256 + b1[10]
+    if dig_P3 > 32767 :
+        dig_P3 -= 65536
+    dig_P4 = b1[13] * 256 + b1[12]
+    if dig_P4 > 32767 :
+        dig_P4 -= 65536
+    dig_P5 = b1[15] * 256 + b1[14]
+    if dig_P5 > 32767 :
+        dig_P5 -= 65536
+    dig_P6 = b1[17] * 256 + b1[16]
+    if dig_P6 > 32767 :
+        dig_P6 -= 65536
+    dig_P7 = b1[19] * 256 + b1[18]
+    if dig_P7 > 32767 :
+        dig_P7 -= 65536
+    dig_P8 = b1[21] * 256 + b1[20]
+    if dig_P8 > 32767 :
+        dig_P8 -= 65536
+    dig_P9 = b1[23] * 256 + b1[22]
+    if dig_P9 > 32767 :
+        dig_P9 -= 65536
+
+    # BMP280 address, 0x76(118)
+    # Select Control measurement register, 0xF4(244)
+    #		0x27(39)	Pressure and Temperature Oversampling rate = 1
+    #					Normal mode
+    bus4.write_byte_data(0x76, 0xF4, 0x27)
+    # BMP280 address, 0x76(118)
+    # Select Configuration register, 0xF5(245)
+    #		0xA0(00)	Stand_by time = 1000 ms
+    bus4.write_byte_data(0x76, 0xF5, 0xA0)
+
+    time.sleep(delay)
+
+    # BMP280 address, 0x76(118)
+    # Read data back from 0xF7(247), 8 bytes
+    # Pressure MSB, Pressure LSB, Pressure xLSB, Temperature MSB, Temperature LSB
+    # Temperature xLSB, Humidity MSB, Humidity LSB
+    data = bus4.read_i2c_block_data(0x76, 0xF7, 8)
+
+    # Convert pressure and temperature data to 19-bits
+    adc_p = ((data[0] * 65536) + (data[1] * 256) + (data[2] & 0xF0)) / 16
+    adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
+
+    # Temperature offset calculations
+    var1 = ((adc_t) / 16384.0 - (dig_T1) / 1024.0) * (dig_T2)
+    var2 = (((adc_t) / 131072.0 - (dig_T1) / 8192.0) * ((adc_t)/131072.0 - (dig_T1)/8192.0)) * (dig_T3)
+    t_fine = (var1 + var2)
+    cTemp = (var1 + var2) / 5120.0
+    fTemp = cTemp * 1.8 + 32
+
+    # Pressure offset calculations
+    var1 = (t_fine / 2.0) - 64000.0
+    var2 = var1 * var1 * (dig_P6) / 32768.0
+    var2 = var2 + var1 * (dig_P5) * 2.0
+    var2 = (var2 / 4.0) + ((dig_P4) * 65536.0)
+    var1 = ((dig_P3) * var1 * var1 / 524288.0 + ( dig_P2) * var1) / 524288.0
+    var1 = (1.0 + var1 / 32768.0) * (dig_P1)
+    p = 1048576.0 - adc_p
+    p = (p - (var2 / 4096.0)) * 6250.0 / var1
+    var1 = (dig_P9) * p * p / 2147483648.0
+    var2 = p * (dig_P8) / 32768.0
+    pressure = (p + (var1 + var2 + (dig_P7)) / 16.0) / 100
+
+    # Output data to screen
+    print("Reading sensor on i2c bus 4")
+    print "Temperature in Celsius : %.2f C" %cTemp
+    print "Temperature in Fahrenheit : %.2f F" %fTemp
+    print "Pressure : %.2f hPa " %pressure +"\n"'''
